f0309664e4f3a79d41a80c6e44350cb2868d6f00
Jose Gonzalez <josegonzalezguilabert@gmail.com>
2021-05-11 19:42:53 +0200

    post pedido

--- marketplace-eoi/src/app/clases/pedido.ts
+++ marketplace-eoi/src/app/clases/pedido.ts
@@ -10,12 +10,21 @@ export class Pedido {
         cantidad:number
     }>;
 
-    constructor(id:number, idUsuario:number, nombre:string, fecha:string,
-        articulos:Array<{id:number, cantidad:number}>){
-            this.id = id;
+    constructor( idUsuario:number, nombre:string, fecha:string,
+        articulos:Array<[number, number]>){
+            
             this.idUsuario = idUsuario;
             this.nombre = nombre;
             this.fecha = fecha;
-            this.articulos = articulos;
+            this.articulos = new Array<{
+                id:number, 
+                cantidad:number
+            }>()
+            for (const articulo of articulos) {
+                this.articulos.push({
+                    id: articulo[0],
+                    cantidad: articulo[1]
+                })
+            }
         }
 }
